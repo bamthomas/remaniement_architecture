8497c02ce6529fcb77c75165471bb269e7bd92aa
Bruno Thomas <bthomas@barreverte.fr>
2011-05-31 22:55:37 +0200

    bug rsync

--- genere_presentation.sh
+++ genere_presentation.sh
@@ -2,9 +2,9 @@
 
 git clone git://github.com/bamthomas/remaniement_architecture.git
 
-rsync -av remaniement_architecture/graphics graphics
-rsync -av remaniement_architecture/styles styles
-rsync -av remaniement_architecture/scripts scripts
+rsync -av remaniement_architecture/graphics/ graphics/
+rsync -av remaniement_architecture/styles/ styles/
+rsync -av remaniement_architecture/scripts/ scripts/
 rsync -av remaniement_architecture/remaniement_architecture.html remaniement_architecture.html
 
 rm -rf remaniement_architecture/
